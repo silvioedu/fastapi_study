a652840b7a3d620628bb96aaaf6fc940d0b4a030
Silvio Silva <silvioedu@gmail.com>
2022-09-17 21:05:42 -0300

    ajuste gitguard

--- secao06/main.py
+++ secao06/main.py
@@ -12,10 +12,3 @@ if __name__ == '__main__':
   import uvicorn
   uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
 
-
-"""
-Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNlc3NfdG9rZW4iLCJleHAiOjE2NjQwMjU4MTAsImlhdCI6MTY2MzQyMTAxMCwic3ViIjoiMiJ9.r4NLbhFCCX_40DdwxfpjE4AHOVu3kkpYeDYJYNarTME
-Type: bearer
-
-Token Ray: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNlc3NfdG9rZW4iLCJleHAiOjE2NjQwNjM0MzksImlhdCI6MTY2MzQ1ODYzOSwic3ViIjoiMyJ9.pJHhQm4fvpEFSndVHnqN1QLIgbFwmTaeeduQJcYcVK8
-"""
